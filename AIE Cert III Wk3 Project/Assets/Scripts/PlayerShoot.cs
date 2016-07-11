157f774f55c11cb6ac96028927ea3cce0da5cfef
Cody Smith <cjsmith2000@hotmail.com>
2016-07-11 14:13:12 +1000

    MADE. GUNS. SHOOT

---
+++ AIE Cert III Wk3 Project/Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShoot : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			if (Physics.Raycast (Camera.main.ViewportPointToRay (new Vector3 (0.5f, 0.5f, 0f))))
+			{
+				print ("you hit a wall");
+			}
+		}
+	}
+}
