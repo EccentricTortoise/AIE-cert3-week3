2a4f1858b1fa0c73b9d5e4dd98666035890a6152
Cody Smith <cjsmith2000@hotmail.com>
2016-07-11 15:58:23 +1000

    gah
    # Conflicts:
    #	AIE Cert III Wk3 Project/Assembly-CSharp.csproj
    #	AIE Cert III Wk3 Project/Assets/game.unity
    #	AIE Cert III Wk3 Project/Library/CurrentLayout.dwlt
    #	AIE Cert III Wk3 Project/Library/assetDatabase3
    #	AIE Cert III Wk3 Project/Library/expandedItems
    #	AIE Cert III Wk3
    Project/Library/metadata/00/00000000000000006100000000000000
    #	AIE Cert III Wk3
    Project/Library/metadata/45/45a39782781b1964e94d1bf3370a8e3d

---
+++ AIE Cert III Wk3 Project/Assets/Scripts/PlayerWeapon.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerWeapon : MonoBehaviour {
+
+	public int weapon = 0;
+    /*
+        0 = pistol
+        1 = machine gun
+    */
+
+	public GameObject pistol;
+	public GameObject machineGun;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		switch (weapon)
+		{
+			case 0:
+				pistol.SetActive (true);
+				machineGun.SetActive (false);
+				break;
+
+			case 1:
+				pistol.SetActive (false);
+				machineGun.SetActive (true);
+				break;
+		}
+	}
+}
