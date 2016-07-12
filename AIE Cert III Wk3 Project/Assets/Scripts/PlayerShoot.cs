22d15723db7800dd11ffebc91d970b666deafab9
Cody Smith <cjsmith2000@hotmail.com>
2016-07-12 10:21:34 +1000

    y

--- AIE Cert III Wk3 Project/Assets/Scripts/PlayerShoot.cs
+++ AIE Cert III Wk3 Project/Assets/Scripts/PlayerShoot.cs
@@ -3,21 +3,75 @@ using System.Collections;
 
 public class PlayerShoot : MonoBehaviour {
 
+    float timer = 0f;
+    float defTimer = 0.25f;
+
+    bool rapidFire = false;
+
+    PlayerWeapon playerWeapon;
+
 	// Use this for initialization
 	void Start ()
 	{
-	
+        playerWeapon = GetComponent<PlayerWeapon>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		if (Input.GetMouseButtonDown(0))
-		{
-			if (Physics.Raycast (Camera.main.ViewportPointToRay (new Vector3 (0.5f, 0.5f, 0f))))
-			{
-				print ("you hit a wall");
-			}
-		}
+        switch (playerWeapon.weapon)
+        {
+            case 0:
+                defTimer = 0.25f;
+                rapidFire = false;
+                break;
+
+            case 1:
+                defTimer = 0.05f;
+                rapidFire = true;
+                break;
+
+            case 2:
+                defTimer = 0.2f;
+                rapidFire = false;
+                break;
+        }
+
+        timer += Time.deltaTime;
+
+        if (rapidFire)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                if (timer >= defTimer)
+                {
+                    timer = 0;
+                    if (Physics.Raycast(Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f))))
+                    {
+                        print("you hit a wall");
+                    }
+                }
+            }
+        }
+        else
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (timer >= defTimer)
+                {
+                    timer = 0;
+                    if (Physics.Raycast(Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f))))
+                    {
+                        print("you hit a wall");
+                    }
+                }
+            }
+        }
 	}
+
+    void OnGUI()
+    {
+        GUI.TextField(new Rect(10, 52, 200, 20), "delay " + timer + " (max is " + defTimer + ")", 100);
+        GUI.TextField(new Rect(10, 71, 200, 20), "rapidFire " + rapidFire, 100);
+    }
 }
