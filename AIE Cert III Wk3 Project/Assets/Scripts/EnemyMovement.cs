c27c2a720fbe71c88ef13bb5b8361ae5169eb201
Lucas Turnbull <link.turnbull@gmail.com>
2016-07-12 14:20:52 +1000

    Added basic enemy AI.

---
+++ AIE Cert III Wk3 Project/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour {
+	
+	public float m_CloseDistance = 0.1f;
+	
+	private GameObject m_Player;
+    private NavMeshAgent m_NavAgent;
+    private Rigidbody m_Rigidbody;
+	
+	private bool m_Follow;
+
+    private void Awake()
+    {
+        m_Player = GameObject.FindGameObjectWithTag("Player");
+        m_NavAgent = GetComponent<NavMeshAgent>();
+        m_Rigidbody = GetComponent<Rigidbody>();
+        m_Follow = false;
+    }
+	
+	private void OnEnable()
+    {
+        m_Rigidbody.isKinematic = false;
+    }
+
+    private void OnDisable()
+    {
+        m_Rigidbody.isKinematic = true;
+    }
+	
+	private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(" Player in range");
+        m_Follow = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log(" Player out of range");
+        m_Follow = true;
+    }
+	
+	void Update ()
+    {
+        if (m_Follow == false)
+            return;
+
+        float distance = (m_Player.transform.position - transform.position).magnitude;
+        if (distance > m_CloseDistance)
+        {
+            Debug.Log("Following Player");
+            Debug.Log(m_Player.transform.position);
+            m_NavAgent.SetDestination(m_Player.transform.position);
+            m_NavAgent.Resume();
+        }
+		else if (m_NavAgent.transform.position != m_Player.transform.position)
+		{
+			m_NavAgent.SetDestination(m_Player.transform.position);
+			m_NavAgent.Resume();
+		}
+        else
+        {
+            m_NavAgent.Stop();
+        }
+		
+	}
+}
