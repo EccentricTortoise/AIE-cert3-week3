7c65fa4d2b27fdb6e2072c6bd5a5630368946845
Cody Smith <cjsmith2000@hotmail.com>
2016-07-12 11:07:24 +1000

    metadata is sucks

---
+++ AIE Cert III Wk3 Project/Assets/Scripts/LockCursor.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LockCursor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+		Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    if (Input.GetKeyDown (KeyCode.Escape))
+        {
+            if (Cursor.lockState == CursorLockMode.Locked)
+            {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+            }
+        }
+	}
+}
---
+++ AIE Cert III Wk3 Project/Assets/models/AttackBot/left-wheel-spin-animation.js
@@ -0,0 +1,28 @@
+﻿function Update() {
+    // Slowly rotate the object around its X axis at 1 degree/second.
+
+    //moving forward + boosting
+    if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.LeftShift)) {
+        transform.Rotate(0, Time.deltaTime * 200, 0);
+        //Debug.Log("Boosting");
+    }
+    else if (Input.GetKey(KeyCode.W)) {
+        transform.Rotate(0, Time.deltaTime * 100, 0);
+        //Debug.Log("Normal Speed");
+    }
+    //moving backwards + boosting
+    if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.LeftShift)) {
+        transform.Rotate(0, Time.deltaTime * -200, 0);
+    }
+    else if (Input.GetKey(KeyCode.S)) {
+        transform.Rotate(0, Time.deltaTime * -100, 0);
+    }
+
+    //turning
+    if (Input.GetKey(KeyCode.D)) {
+        transform.Rotate(0, Time.deltaTime * 100, 0);
+    }
+
+
+
+}
